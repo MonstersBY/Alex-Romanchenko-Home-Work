7ca5d9ba4fd5aca57ba7754f28e0cbe8b5f7c24e
MonstersBY <monsters_alex@mail.ru>
2018-03-26 20:48:32 +0300

    final
    final

--- final task/All.sql
+++ final task/All.sql
@@ -67,3 +67,4 @@ update employees
 set salary=salary*1.1
 where department_id in (select department_id from(select department_id,count(department_id) as numb from employees group by department_id) as countTable
 where numb>10);
+commit;
